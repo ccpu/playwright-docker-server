be826a2d2f48fe772418db0c98fae3ad76ae84bf
cyrus <mdoaie@yahoo.co.uk>
2024-10-20 14:01:26 +0100

    remove duplicate add Dockerfile.playwright.1.17.2 to fix playwright installation

--- src/utils/__tests__/index.test.ts
+++ src/utils/__tests__/index.test.ts
@@ -1,7 +1,6 @@
 import {
   getBrowserType,
   getLaunchOptions,
-  launchOptions,
   extractProcessEnvOptions,
 } from '../index';
 
@@ -9,7 +8,6 @@ describe('getBrowserType', () => {
   it('should be defined', () => {
     expect(getBrowserType).toBeDefined();
     expect(getLaunchOptions).toBeDefined();
-    expect(launchOptions).toBeDefined();
     expect(extractProcessEnvOptions).toBeDefined();
   });
 });
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,4 @@
 import './trim-special-char';
 export * from './browser-type';
 export * from './launch-options';
-export * from './launch-options';
-export * from './get-playwright-version';
 export * from './get-playwright-version';
